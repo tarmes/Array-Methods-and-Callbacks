619d20c69dfa96de1cbf2ba4075856f31bae3b64
tarmes <traarmes@gmail.com>
2020-07-21 16:56:31 -0400

    Task 1

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ console.log('its working');
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
 
+const home2014Fin = fifaData.filter(function(item){
+    return item.Year === 2014 && item.Stage === 'Final';
+});
+
+console.log(home2014Fin[0]['Home Team Name']);
+console.log(home2014Fin[0]['Away Team Name']);
+console.log(home2014Fin[0]['Home Team Goals']);
+console.log(home2014Fin[0]['Away Team Goals']);
+console.log(home2014Fin[0]['Home Team Name']);
+
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
