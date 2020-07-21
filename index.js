e320aa058b5413046a7c50539a4c1d2a4c672fc9
tarmes <traarmes@gmail.com>
2020-07-21 17:06:48 -0400

    Task 2

--- index.js
+++ index.js
@@ -25,11 +25,15 @@ console.log(home2014Fin[0]['Home Team Name']);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+function getFinals(data) {
+    return data.filter(function(filterFinals){
+        return filterFinals.Stage === 'Final';
+    });
+}
 
     /* code here */
+console.log(getFinals(fifaData));
 
-};
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
